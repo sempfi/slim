c88f333d6a7087a4bc012353acc81a18cc2da857
Alireza Rostami <71198002+sempfi@users.noreply.github.com>
2025-08-13 07:35:50 +0330

    Update prune.py

--- slim/prune.py
+++ slim/prune.py
@@ -538,12 +538,12 @@ def prune_pruner_zero(
     model.config.use_cache = False
 
     print("loading calibdation data")
-    dataloader, _ = get_loaders("c4", nsamples=nsamples, seed=seed, seqlen=model.model.config.max_position_embeddings, tokenizer=tokenizer)
+    dataloader, _ = get_loaders("c4", nsamples=nsamples, seed=seed, seqlen=model.config.max_position_embeddings, tokenizer=tokenizer)
     print("dataset loading complete")
     with torch.no_grad():
         inps, outs, attention_mask, position_ids = prepare_calibration_input(model, dataloader, device)
 
-    layers = model.model.layers
+    layers = model.layers
     
     progress_bar = tqdm.tqdm(range(len(layers)))
 
